0ac087b3510ef017b052eb328a930cccc39a164d
weygerus <gabrileao38@gmail.com>
2022-03-21 16:24:20 -0300

    21/03/2022-2

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses/courses.component';
+import { courseListComponent } from './courses/course-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    courseListComponent
   ],
   imports: [
     BrowserModule,
--- projeto/src/app/courses/course-list.component.ts
+++ projeto/src/app/courses/course-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ICourses } from './course';
 
 @Component({
     selector:'app-course-list',
-    template:'',
-    styles
+    templateUrl:'./course-list.component.html'
 })
 
-export class courseListComponent {
+export class courseListComponent implements OnInit {
+    
+    courses : ICourses[] = [];
 
+    ngOnInit(): void {
+
+        this.courses = [
+            {
+                id       : 1,
+                name     : 'Introdução ao Angular 12',
+                imageUrl : '',
+                price    : 100,
+                code     : 'exemplo-1',
+                duration : 20,
+                rating   : 4.8, 
+            },
+            {
+                id       : 2,
+                name     : 'Introdução ao HTML 5',
+                imageUrl : '',
+                price    : 100,
+                code     : 'exemplo-2',
+                duration : 20,
+                rating   : 4.5, 
+            },
+        ]    
+    }
 }
